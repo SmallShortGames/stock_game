e06d6c95ffceecb2eb96c97aa849fc1a648fe648
SMKrizan <smkrizan@gmail.com>
2021-04-02 11:42:17 -0500

    set up Flask server

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,14 @@
-from flask import flask
+from flask import Flask
 
+def create_app(test_config=None):
+    app = Flask(__name__, static_url_path='/')
+    app.url_map.strict_slashes = False
+    app.config.from_mapping(
+        SECRET_KEY='noone_knows'
+    )
+
+    @app.route('/hello')
+    def hello():
+        return 'test connect'
+
+    return app
