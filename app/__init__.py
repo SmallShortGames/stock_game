e5c18844ea72096348b66879fa5fd402cb095e48
SMKrizan <smkrizan@gmail.com>
2021-04-15 15:52:45 -0500

    .env added markupsafe imported & user route added

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
-
+from markupsafe import escape
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/hello')
 def hello():
-    return {'key':"value"}
+    return {"key": "value"}
+
+# returns user profile
+@app.route('/user/<username>')
+def user_profile(username):
+    return 'Welcome, %!' % escape(username)
 
 @app.route('/buy')
 def buy():
