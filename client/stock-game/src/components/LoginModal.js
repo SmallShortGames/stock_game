2f80c925b572e38dc9f9e53915af31ebedca5f20
patrickfinnigan <patrickfinnigan94@gmail.com>
2021-06-11 16:26:58 -0700

    added modal forms

--- client/stock-game/src/components/LoginModal.js
+++ client/stock-game/src/components/LoginModal.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Modal from "react-bootstrap/Modal";
+import Form from 'react-bootstrap/Form';
 import Button from "react-bootstrap/Button";
 
 export default function LoginModal() {
@@ -16,19 +17,36 @@ export default function LoginModal() {
 
       <Modal show={show} onHide={handleClose} centered>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Login</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer className="justify-content-center">
+          <Button variant="primary" onClick={handleClose}>
+            Forgot Username
           </Button>
           <Button variant="primary" onClick={handleClose}>
-            Save Changes
+            Forgot Password
           </Button>
         </Modal.Footer>
       </Modal>
     </>
   );
 }
-
--- client/stock-game/src/components/RegistrationModal.js
+++ client/stock-game/src/components/RegistrationModal.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 export default function RegistrationModal() {
@@ -16,19 +17,40 @@ export default function RegistrationModal() {
 
       <Modal show={show} onHide={handleClose} centered>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Register Account</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formRegistrationUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="email" placeholder="Enter username" />
+            </Form.Group>
+            <Form.Group controlId="formRegistrationEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formRegistrationPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox"></Form.Group>
+            <Form.Group controlId="formRegistrationPasswordConfirm">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox"></Form.Group>
+            <Form.Check type="checkbox" label="I am 18 or older" />
+          </Form>
+        </Modal.Body>
+        <Modal.Footer className="justify-content-center">
           <Button variant="primary" onClick={handleClose}>
-            Save Changes
+            Submit
           </Button>
         </Modal.Footer>
       </Modal>
     </>
   );
 }
-
