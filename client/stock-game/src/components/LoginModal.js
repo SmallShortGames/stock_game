47cf8198267400344f52e1522536a01de5078ca9
gregstead <gregstead@gmail.com>
2021-07-10 14:37:05 -0500

    refactor login to use rjsf

--- client/stock-game/src/components/LoginModal.js
+++ client/stock-game/src/components/LoginModal.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
-import { useFormik } from "formik";
-import * as Yup from "yup";
 
 import Modal from "react-bootstrap/Modal";
-import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+
+import LoginForm from "./LoginForm.jsx";
 
 export default function LoginModal() {
   const [show, setShow] = useState(false);
@@ -12,84 +11,24 @@ export default function LoginModal() {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const formik = useFormik({
-    initialValues: {
-      loginEmail: "",
-      password: "",
-    },
-
-    validationSchema: Yup.object({
-      loginEmail: Yup.string().email("Invalid email").required("Required"),
-      password: Yup.string().required(),
-    }),
-
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
-
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
+      <Button variant="danger" onClick={handleShow}>
         Login Modal
       </Button>
 
       <Modal show={show} onHide={handleClose} centered>
         <Modal.Header closeButton>
-          <Modal.Title>Login</Modal.Title>
+          <Modal.Title>Register Account</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form onSubmit={formik.handleSubmit} noValidate>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                required
-                id="loginEmail"
-                name="loginEmail"
-                type="email"
-                onChange={formik.handleChange}
-                value={formik.values.loginEmail}
-                onBlur={formik.handleBlur}
-                placeholder="Enter Email"
-              />
-              {formik.touched.loginEmail && formik.errors.loginEmail ? (
-                <div style={{ color: "red" }}>Enter email</div>
-              ) : null}
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                required
-                id="password"
-                name="password"
-                type="string"
-                onChange={formik.handleChange}
-                value={formik.values.password}
-                onBlur={formik.handleBlur}
-                placeholder="Enter Password"
-              />
-              {formik.touched.password && formik.errors.password ? (
-                <div style={{ color: "red" }}>Enter password</div>
-              ) : null}
-            </Form.Group>
-            <Form.Group controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Form.Group className="mx-auto ml-3">
-              <Button type="submit">Submit</Button>{" "}
-              <Button variant="primary" onClick={handleClose}>
-                Forgot Username
-              </Button>{" "}
-              <Button variant="primary" onClick={handleClose}>
-                Forgot Password
-              </Button>{" "}
-            </Form.Group>
-          </Form>
+          <LoginForm />
         </Modal.Body>
+        <Modal.Footer className="justify-content-center">
+          <Button variant="primary" onClick={handleClose}>
+            Submit
+          </Button>
+        </Modal.Footer>
       </Modal>
     </>
   );
--- client/stock-game/src/views/LoginPage/LoginPage.js
+++ client/stock-game/src/views/LoginPage/LoginPage.js
@@ -1,8 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { useFormik } from "formik";
-import * as Yup from "yup";
-import API from "../../utils/API";
+import { Link, useHistory } from "react-router-dom";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -14,46 +11,43 @@ import Tabs from "react-bootstrap/Tabs";
 import Tab from "react-bootstrap/Tab";
 
 import Navbar from "../../components/Navbar.js";
-import LoginModal from "../../components/LoginModal.js";
-import RegistrationModal from "../../components/RegistrationModal.js";
+import LoginForm from "../../components/LoginForm";
+import { useAuth } from "../../services/userContext";
+import API from "../../utils/API.js";
 
-import "./LoginPage.scss";
+// import "./RegistrationPage.scss";
 
 export default function LoginPage() {
-  const formik = useFormik({
-    initialValues: {
-      loginEmail: "",
-      password: "",
-    },
-
-    validationSchema: Yup.object({
-      loginEmail: Yup.string().email("Invalid email").required("Required"),
-      password: Yup.string().required(),
-    }),
-
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
-
-  const [loginState, setLoginState] = useState({
+  let history = useHistory();
+  const { setAuthTokens } = useAuth();
+  const [registrationState, setRegistrationState] = useState({
+    username: "",
     email: "",
     password: "",
   });
 
   function handleChange(event) {
     const { name, value } = event.target;
-    setLoginState({
-      ...loginState,
+    setRegistrationState({
+      ...registrationState,
       [name]: value,
     });
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    API.userLogin(loginState)
+    API.userRegister(registrationState)
       .then((res) => {
-        console.log(res);
+        if (res.status === 201) {
+          setRegistrationState({
+            username: "",
+            email: "",
+            password: "",
+          });
+          console.log(res);
+          // setAuthTokens(res.data);
+          history.push("/");
+        }
       })
       .catch((err) => {
         console.error(err);
@@ -63,62 +57,12 @@ export default function LoginPage() {
     <>
       <Navbar />
       <Container className="center">
-        <Row className="w-50">
+        <Row className="w-75">
           <Col>
             <Card>
-              <Card.Header>Login Page</Card.Header>
+              <Card.Header>LoginPage</Card.Header>
               <Card.Body>
-                <Form onSubmit={formik.handleSubmit} noValidate>
-                  <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control
-                      required
-                      id="loginEmail"
-                      name="loginEmail"
-                      type="email"
-                      onChange={formik.handleChange}
-                      value={formik.values.loginEmail}
-                      onBlur={formik.handleBlur}
-                      placeholder="Enter Email"
-                    />
-                    {formik.touched.loginEmail && formik.errors.loginEmail ? (
-                      <div style={{ color: "red" }}>Enter Email</div>
-                    ) : null}
-                    <Form.Text className="text-muted">
-                      We'll never share your email with anyone else.
-                    </Form.Text>
-                  </Form.Group>
-                  <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                      required
-                      id="password"
-                      name="password"
-                      type="string"
-                      onChange={formik.handleChange}
-                      value={formik.values.password}
-                      onBlur={formik.handleBlur}
-                      placeholder="Enter Password"
-                    />
-                    {formik.touched.password && formik.errors.password ? (
-                      <div style={{ color: "red" }}>Enter password</div>
-                    ) : null}
-                  </Form.Group>
-                  <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                  </Form.Group>
-                  <Form.Group className="mx-auto ml-3">
-                    <Button variant="primary">Forgot Username</Button>{" "}
-                    <Button variant="primary">Forgot Password</Button>{" "}
-                    <Button onClick={handleSubmit} variant="primary">
-                      Login
-                    </Button>{" "}
-                  </Form.Group>
-                </Form>
-                <br />
-                <Link to="/">
-                  <Button variant="primary">Return to Main Page</Button>
-                </Link>
+                <LoginForm />
               </Card.Body>
             </Card>
           </Col>
