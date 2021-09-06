eb6ca934dc476405830a542002d24ab22ee44ac8
gregstead <gregstead@gmail.com>
2021-09-05 20:11:31 -0500

    mobile-first navbar style

--- client/stock-game/src/layouts/DashboardLayout/index.jsx
+++ client/stock-game/src/layouts/DashboardLayout/index.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import "./style.css";
 
 import Navbar from "../../components/Navbar/";
 
 export default function DashboardLayout({ children }) {
   return (
     <>
-      <header>
-        <Navbar />
-      </header>
-      <main>{children}</main>
+      <div className="page-container">
+        <header className="navbar-container">
+          <Navbar />
+        </header>
+        <main className="main-container">{children}</main>
+      </div>
     </>
   );
 }
